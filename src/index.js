844b05c47385a280d36c66aafedd6e87980db9a0
Mayank Dargan <mayank.dargan@nagarro.com>
2019-06-25 22:25:25 +0530

    functionalities added

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import * as serviceWorker from './serviceWorker';
+// const _ = require('lodash');
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
